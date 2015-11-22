45d095d8332ecc304795ad6d598cef6b0febb955
Jarred Capellman <jcapellman@hotmail.com>
2015-11-21 22:02:37 -0500

    Removed extraneous js/css from the WebAPI Service

--- jcMPP.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace jcMPP.WebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- jcMPP.WebAPI/Global.asax.cs
+++ jcMPP.WebAPI/Global.asax.cs
@@ -14,7 +14,6 @@ namespace jcMPP.WebAPI {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
