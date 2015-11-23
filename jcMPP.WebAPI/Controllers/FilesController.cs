e9591d52b29a4f134dbbc86162931cd89b69218e
Jarred Capellman <jcapellman@hotmail.com>
2015-11-22 21:11:12 -0500

    Started work on the WebAPI Controllers to return the Server Side Files

---
+++ jcMPP.WebAPI/Controllers/FilesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+using jcMPP.PCL.DataLayer.Models;
+using jcMPP.WebAPI.DataLayer.DbContexts;
+
+namespace jcMPP.WebAPI.Controllers {
+    public class FilesController : ApiController {
+        public IEnumerable<Files> Get(List<Guid> clientFiles) {
+            using (var fileContext = new FileContext()) {
+                return fileContext.FilesDS.ToList();
+            }
+        }
+    }
+}
