fa3a721aab42dde3c264f26b6c637a0b346154ae
Jarred Capellman <jcapellman@hotmail.com>
2015-12-01 10:49:25 -1000

    Fixed FilesController

--- jcMPP.WebAPI/Controllers/FilesController.cs
+++ jcMPP.WebAPI/Controllers/FilesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 
 using jcMPP.PCL.DataLayer.Models.Views;
+using jcMPP.PCL.Objects;
 using jcMPP.WebAPI.DataLayer.DbContexts;
 
 namespace jcMPP.WebAPI.Controllers {
@@ -11,12 +12,13 @@ namespace jcMPP.WebAPI.Controllers {
         // Client Sends Files to server and then the server returns all files the client doesn't have
         [HttpPost]
         public CTO<List<GetActiveFilesVIEW>> Get(List<Guid> clientFiles) {
-		try {
-            using (var fileContext = new FileContext()) {
-                return new CTO(fileContext.ActiveFilesDS.Where(a => !clientFiles.Contains(a.ID)).ToList());
+            try {
+                using (var fileContext = new FileContext()) {
+                    return new CTO<List<GetActiveFilesVIEW>>(fileContext.ActiveFilesDS.Where(a => !clientFiles.Contains(a.ID)).ToList());
+                }
+            } catch (Exception ex) {
+                return new CTO<List<GetActiveFilesVIEW>>(null, ex.ToString());
             }
-} catch (Exception ex) {
-		return new CTO<List<GetActiveFilesVIEW>>(null, ex.ToString());
         }
     }
 }
