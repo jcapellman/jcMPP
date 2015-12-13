98d16ee3b8b1f33501c7c2f146415dc30dbd82c5
Jarred Capellman <jcapellman@hotmail.com>
2015-12-13 14:33:36 -0500

    Added Landing Icons and Navigation

--- jcMPP.UWP/Views/LandingPage.xaml.cs
+++ jcMPP.UWP/Views/LandingPage.xaml.cs
@@ -1,10 +1,23 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace jcMPP.UWP.Views {
 
     public sealed partial class LandingPage : Page {
         public LandingPage() {
             this.InitializeComponent();
+        }
+
+        private void BtnPortScan_OnClick(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(PortScanPage));
+        }
+
+        private void BtnHash_OnClick(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(HashCrackerPage));
+        }
+
+        private void BtnSettings_OnClick(object sender, RoutedEventArgs e) {
+            this.Frame.Navigate(typeof(SettingsPage));
         }
     }
 }
