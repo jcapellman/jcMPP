5253f9ecb3e0f0d66e6a0eca9710a5e8d928fb19
Jarred Capellman <jcapellman@hotmail.com>
2015-12-27 22:36:39 -0500

    Added initial work on the  background task for the Keep Alive

---
+++ jcMPP.UWP.BackgroundTask/KeepAliveBackgroundTask.cs
@@ -0,0 +1,9 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace jcMPP.UWP.BackgroundTask {
+    public sealed class KeepAliveBackgroundTask : IBackgroundTask {
+        public void Run(IBackgroundTaskInstance taskInstance) {
+
+        }
+    }
+}
