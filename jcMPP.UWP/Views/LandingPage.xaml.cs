663875d9542efad149e1dda4e423723094c78b6a
Jarred Capellman <jcapellman@hotmail.com>
2015-12-31 21:52:38 -0800

    Fix: BackgroundTask Registration now working properly

--- jcMPP.UWP/Views/LandingPage.xaml.cs
+++ jcMPP.UWP/Views/LandingPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 using Windows.ApplicationModel.Background;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 using jcMPP.UWP.BackgroundTask;
 
 namespace jcMPP.UWP.Views {
@@ -10,20 +12,28 @@ namespace jcMPP.UWP.Views {
     public sealed partial class LandingPage : Page {
         public LandingPage() {
             this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e) {
+            var backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
 
             var keepAliveBGName = "KeepAliveBackgroundTask";
 
             var taskRegistered = BackgroundTaskRegistration.AllTasks.Any(task => task.Value.Name == keepAliveBGName);
 
-            //if (!taskRegistered) {
-            //    var builder = new BackgroundTaskBuilder {
-            //        Name = keepAliveBGName,
-            //        IsNetworkRequested = true,
-            //        TaskEntryPoint = typeof(KeepAliveBackgroundTask).FullName
-            //    };
-                
-            //    var task = builder.Register();
-            //}
+            if (!taskRegistered) {
+                var builder = new BackgroundTaskBuilder {
+                    Name = keepAliveBGName,
+                    IsNetworkRequested = true,
+                    TaskEntryPoint = typeof(KeepAliveBackgroundTask).FullName
+                };
+
+                var trigger = new TimeTrigger(20, false);
+
+                builder.SetTrigger(trigger);
+
+                var task = builder.Register();
+            }
         }
 
         private void BtnPortScan_OnClick(object sender, RoutedEventArgs e) {
