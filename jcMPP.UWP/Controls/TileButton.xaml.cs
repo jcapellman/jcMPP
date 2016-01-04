fa3fca5c42fc5b11c86f722ebceca743753932bb
Jarred Capellman <jcapellman@hotmail.com>
2016-01-03 21:27:53 -0800

    Initial work on the TileButton control

---
+++ jcMPP.UWP/Controls/TileButton.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace jcMPP.UWP.Controls {
+    public sealed partial class TileButton : UserControl {
+        public TileButton() {
+            this.InitializeComponent();
+        }
+
+        private string _caption;
+
+        protected string Caption
+        {
+            get { return _caption; }
+
+            set { _caption = value; }
+        }
+
+        private Uri _icon;
+
+        protected Uri Icon
+        {
+            get { return _icon; }
+
+            set { _icon = value; }
+        }
+    }
+}
