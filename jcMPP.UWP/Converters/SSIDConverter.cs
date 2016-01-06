f92f93ca5da5b7e0003a4862706690c5708d91c8
Jarred Capellman <jcapellman@hotmail.com>
2016-01-05 22:12:40 -0500

    Added: Hidden Networks now show their Bssid

---
+++ jcMPP.UWP/Converters/SSIDConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Windows.Devices.WiFi;
+using Windows.UI.Xaml.Data;
+
+namespace jcMPP.UWP.Converters {
+    public class SSIDConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if (value == null || parameter == null) {
+                return null;
+            }
+
+            var network = (WiFiAvailableNetwork) value;
+
+            return string.IsNullOrEmpty(network.Ssid) ? $"Hidden Network ({network.Bssid})" : network.Ssid;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
