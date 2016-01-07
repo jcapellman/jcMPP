913563faca3f0c2f9d012d74f272a96f0340c4db
Jarred Capellman <jcapellman@hotmail.com>
2016-01-06 19:45:27 -0500

    Added: TimespanConverter for WifiScanning

---
+++ jcMPP.UWP/Converters/TimespanConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Windows.UI.Xaml.Data;
+
+namespace jcMPP.UWP.Converters {
+    public class TimespanConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if (value == null || parameter == null) {
+                return null;
+            }
+
+            var ts = (TimeSpan) value;
+
+            var str = string.Empty;
+
+            if (ts.TotalDays > 0) {
+                str += $"{ts.TotalDays} days";
+            }
+
+            if (ts.TotalHours > 0) {
+                str += $"{ts.TotalHours} hours";
+            }
+
+            if (ts.TotalMinutes > 0) {
+                str += $"{ts.TotalMinutes} minutes";
+            }
+
+            return str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            throw new NotImplementedException();
+        }
+    }
+}
