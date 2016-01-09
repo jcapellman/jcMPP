4da2c7b6b69bdf7bba6eaf51e9856fd87007b38c
Jarred Capellman <jcapellman@hotmail.com>
2016-01-08 21:06:26 -0500

    Added: AboutPage to the app

---
+++ jcMPP.UWP/ViewModels/AboutModel.cs
@@ -0,0 +1,7 @@
+﻿using jcMPP.PCL.PlatformAbstractions;
+
+namespace jcMPP.UWP.ViewModels {
+    public class AboutModel : BaseModel {
+        public AboutModel(BaseFileIO baseFileIO) : base(baseFileIO) { }
+    }
+}
---
+++ jcMPP.UWP/Views/AboutPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using jcMPP.UWP.Library.PlatformImplementations;
+using jcMPP.UWP.ViewModels;
+
+namespace jcMPP.UWP.Views {
+    public sealed partial class AboutPage : BasePage {
+        public AboutPage() : base(typeof(AboutModel), new UWPFileIO(App.AppSetting)) {
+            this.InitializeComponent();
+        }
+    }
+}
