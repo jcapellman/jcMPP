10d76cd092d7353dfe0d17bd7bf56f7669ecaca2
Jarred Capellman <jcapellman@hotmail.com>
2016-01-09 21:36:10 -0500

    Added Version pulled from the Package instead of hard coding it

--- jcMPP.UWP/ViewModels/AboutModel.cs
+++ jcMPP.UWP/ViewModels/AboutModel.cs
@@ -1,7 +1,28 @@
-﻿using jcMPP.PCL.PlatformAbstractions;
+﻿using System.Linq;
+using System.Reflection;
+
+using Windows.UI.Xaml;
+using Windows.ApplicationModel;
+
+using jcMPP.PCL.PlatformAbstractions;
 
 namespace jcMPP.UWP.ViewModels {
     public class AboutModel : BaseModel {
         public AboutModel(BaseFileIO baseFileIO) : base(baseFileIO) { }
+
+        private string _versionString;
+
+        public string VersionString {
+            get { return _versionString; }
+
+            set { _versionString = value; OnPropertyChanged(); }
+        }
+
+        public void LoadData()
+        {
+            var version = Package.Current.Id.Version;
+
+            VersionString = $"Version {version.Major}.{version.Minor}.{version.Revision}.{version.Build}";
+        }
     }
 }
--- jcMPP.UWP/Views/AboutPage.xaml.cs
+++ jcMPP.UWP/Views/AboutPage.xaml.cs
@@ -1,10 +1,16 @@
-﻿using jcMPP.UWP.Library.PlatformImplementations;
+﻿using Windows.UI.Xaml.Navigation;
+
+using jcMPP.UWP.Library.PlatformImplementations;
 using jcMPP.UWP.ViewModels;
 
 namespace jcMPP.UWP.Views {
     public sealed partial class AboutPage : BasePage {
         public AboutPage() : base(typeof(AboutModel), new UWPFileIO(App.AppSetting)) {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            viewModel<AboutModel>().LoadData();
         }
     }
 }
